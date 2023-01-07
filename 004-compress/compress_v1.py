db5e064303642837fadec4bfb3910217c29f06d1
Agatha Bahati <agathamakori38@gmail.com>
2023-01-07 13:35:46 +0300

    Create compress_v2

---
+++ 004-compress/compress_v1.py
@@ -0,0 +1,33 @@
+def compress(s):
+    s += '@'
+    result=''
+    i=0
+    j=0
+    while j<len(s):
+        # s='ccaaatsss'
+        if s[i] == s[j]:
+            j+=1
+        else:
+            num=j-i
+            if num==1:
+                result+=s[i]
+            else:
+                result += str(num) + s[i]
+            i=j
+
+    return result
+
+print(compress('ccaaatsss'))
+
+"""
+Notes:
+Time complexity: O(n^2)
+This is a less efficient solution:
+Strings in python are immutable therefore concatenating strings
+involves creating a copy of the string to new memory then make the addition.
+This operation has linear time complexity. 
+This time complexity of this algorithm is quadratic because we're running n iterations 
+with a nested linear time from the concatenation operations on the strings.
+
+Also, adding the '@' token is a neat trick!
+"""
