6d28f76edfe8096315201b9fd5111ea268c32864
Agatha Bahati <agatha@marketforce360.com>
2023-02-17 17:46:12 +0300

    test: add test for max value and update max_value_v1

--- max_value/max_value_v1.py
+++ max_value/max_value_v1.py
@@ -2,8 +2,9 @@ def max_value(nums):
   maximum = float('-inf')#to cater to negative integers
   for x in nums:
     if x>maximum:
-      max_value=x
+      maximum=x
       
-  return max_value
+  return maximum
+
 
 # TODO: Add time and space complexity
