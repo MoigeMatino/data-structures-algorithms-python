efe246cfc5bf436078dd98d2330d3847da2010f9
Agatha Bahati <agathamakori38@gmail.com>
2023-02-17 15:16:24 +0300

    test: add test for hey_programmer

---
+++ tests/test_hey_programmer.py
@@ -0,0 +1,13 @@
+import pytest
+from hey_programmer.fstring import greet
+
+test_case=[
+    ('alvin','hey alvin'),
+    ('jason', 'hey jason'),
+    ('how now brown cow', 'hey how now brown cow'),
+    
+]
+
+def test_hey_programmer():
+    for test in test_case:
+        assert greet(test[0])==test[1]
