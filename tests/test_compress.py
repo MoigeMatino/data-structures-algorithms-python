39cc94494ec4a97cf3368ff48524771651dbe61f
Agatha Bahati <agathamakori38@gmail.com>
2023-02-17 14:59:36 +0300

    tests: add test for compress

---
+++ tests/test_compress.py
@@ -0,0 +1,14 @@
+import pytest
+from compress.compress_v1 import compress
+
+test_case = [
+    ('ccaaatsss','2c3at3s'),
+    ('ssssbbz','4s2bz'),
+    ('ppoppppp','2po5p'),
+    ('nnneeeeeeeeeeeezz','3n12e2z'),
+    ('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy','127y')
+]
+
+def test_compress():
+    for test in test_case:
+        assert compress(test[0])==test[1]
