e0565e0df1af481fa5b93e04c3dafd5c804312ad
Agatha Bahati <agatha@marketforce360.com>
2023-02-27 23:22:30 +0300

    refactor: add comments to v3

--- most-frequent-char/most_frequent_char_v3.py
+++ most-frequent-char/most_frequent_char_v3.py
@@ -6,3 +6,9 @@ def most_frequent_char(s):
         else:
             tracker[char] = 1
     return max(tracker,key=tracker.get)
+
+"""
+the return expression finds the key with the maximum value
+Another way to find the key with the maximum value:
+- max(tracker, key=lambda x:tracker[x])
+"""
