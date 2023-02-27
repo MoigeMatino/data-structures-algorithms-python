2dee654f6880d018bfa8c42402f7668fde2ec85d
Agatha Bahati <agatha@marketforce360.com>
2023-02-27 23:13:50 +0300

    feat: add most frequent char v1

---
+++ most-frequent-char/most_frequent_char_v1.py
@@ -0,0 +1,21 @@
+def most_frequent_char(s):
+    tracker = {}
+    for char in s:
+        if char in s:
+            tracker[char] +=1
+        tracker[char] = 1
+
+    proxy=None
+    for char in s:
+        if not proxy or tracker[char] > tracker[proxy]:
+            proxy=char
+
+    return proxy
+
+"""
+
+    n = length of string
+    Time: O(n)
+    Space: O(n)
+
+"""
