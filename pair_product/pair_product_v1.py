29cd8747165d24b96436c3918750d6e9816832b9
Agatha Bahati <agatha@marketforce360.com>
2023-03-22 11:39:32 +0300

    test: add test for pair product

--- pair_product/pair_product_v1.py
+++ pair_product/pair_product_v1.py
@@ -1,8 +1,9 @@
 def pair_product(numbers, target_product):
     nums_dict={}
     for index, num in enumerate(numbers):
-        if quotient:=target_product/num in nums_dict and nums_dict[quotient] != index:
-            return index,nums_dict[quotient]
+        quotient=target_product/num
+        if quotient in nums_dict:
+            return nums_dict[quotient], index
         nums_dict[num]=index
 
 """
