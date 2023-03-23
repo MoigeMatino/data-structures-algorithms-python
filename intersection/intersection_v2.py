e49382a0cd7004fd9b054b046b28dbc1bbd6c9f8
Agatha Bahati <agatha@marketforce360.com>
2023-03-23 20:39:11 +0300

    chore: add time and space complexity for v1 and v2

--- intersection/intersection_v2.py
+++ intersection/intersection_v2.py
@@ -2,3 +2,10 @@ def intersection(a,b):
     my_set=set(a)
     return [element for element in b if element in my_set]
 
+"""
+This approach uses the set pass
+Again, using a set is more efficient because it has O(1)
+lookup time
+Time: O(n+m)
+Space: O(n)
+"""
