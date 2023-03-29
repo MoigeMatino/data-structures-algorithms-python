59d8a6cf37fceda907ca855e8826359bb8e32170
Agatha Bahati <agatha@marketforce360.com>
2023-03-29 11:46:45 +0300

    add time and space complexity to v2

--- linked_lists/sum_list/sum_list_v2.py
+++ linked_lists/sum_list/sum_list_v2.py
@@ -2,3 +2,9 @@ def sum_list(head):
     if head is None:
         return 0
     return head.val + sum_list(head.next)
+
+"""
+n: number of nodes in linked list
+Time: O(n)
+Space: O(n); this is because of the memory 
+"""
