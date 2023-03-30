261815275f9b24050005733ed3cb945061b23c89
Agatha Bahati <agatha@marketforce360.com>
2023-03-30 20:26:56 +0300

    feat: add iterative approach

---
+++ linked_lists/get_node_value/get_node_value_v1.py
@@ -0,0 +1,10 @@
+def get_node_value(head, index):
+    counter = 0
+    current = head
+    while current is not None:
+        if counter == index:
+            return current.val
+        current = current.next
+        counter += 1
+        
+    return None
