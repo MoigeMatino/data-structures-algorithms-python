5eeac545dee8dd808c9e35ac4051e0686dc21705
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-04-07 21:15:11 +0300

    feat: add zipper lists v1 (#54)
    Co-authored-by: Agatha Bahati <agatha@marketforce360.com>

---
+++ linked_lists/zipper_lists/zipper_lists_v1.py
@@ -0,0 +1,23 @@
+def zipper_lists(head_1, head_2):
+    tracker = head_1
+    current_1 = head_1.next
+    current_2 = head_2
+    count = 0
+    
+    while current_1 is not None and current_2 is not None:
+        if count % 2 == 0:
+            tracker.next = current_2
+            current_2 = current_2.next
+        else:
+            tracker.next = current_1
+            current_1 = current_1.next
+
+        tracker = tracker.next
+        count += 1
+
+    if current_1 is not None:
+        tracker.next = current_1
+    if current_2 is not None:
+        tracker.next = current_2
+
+    return head_1
