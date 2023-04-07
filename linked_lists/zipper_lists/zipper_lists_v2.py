c47de429f5238b0a0cbb9df6b02471bc4502f306
Agatha Bahati <agatha@marketforce360.com>
2023-04-07 21:21:28 +0300

    feat: add zipper lists v2

---
+++ linked_lists/zipper_lists/zipper_lists_v2.py
@@ -0,0 +1,17 @@
+def zipper_lists(head_1, head_2):
+
+    if head_1 is None and head_2 is None:
+        return None
+    
+    if head_1 is None:
+        return head_2
+    
+    if head_2 is None:
+        return head_1
+    
+    next_1 = head_1.next
+    next_2 = head_2.next
+    head_1.next = head_2
+    head_2.next = zipper_lists(next_1, next_2)
+    return head_1
+
