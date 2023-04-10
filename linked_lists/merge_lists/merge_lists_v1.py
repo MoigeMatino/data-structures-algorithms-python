829c60605187c56c48b922b8e6f8d53f5156775f
Agatha Bahati <agatha@marketforce360.com>
2023-04-10 12:21:36 +0300

    feat: add merge lists v1 and v2

---
+++ linked_lists/merge_lists/merge_lists_v1.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def merge_lists(head_1, head_2):
+    dummy_head = Node(None)
+    tracker = dummy_head
+    while head_1 is not None and head_2 is not None:
+        if head_1.val < head_2.val:
+            tracker.next = head_1
+            head_1 = head_1.next
+        else:
+            tracker.next = head_2
+            head_2 = head_2.next
+        
+        tracker = tracker.next
+    
+    if head_1 is not None:
+        tracker.next = head_1
+    if head_2 is not None:
+        tracker.next = head_2
+
+    return dummy_head.next
---
+++ linked_lists/merge_lists/merge_lists_v2.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def merge_lists(head_1, head_2):
+    if head_1 is None and head_2 is None:
+        return None
+    if head_1 is None:
+        return head_2
+    if head_2 is None:
+        return head_1
+    if head_1.val < head_2.val:
+        next_1 = head_1.next
+        head_1.next = merge_lists(next_1, head_2)
+        return head_1
+    else: 
+        next_2 = head_2.next
+        head_2 = merge_lists(head_1, next_2)
+        return head_2
