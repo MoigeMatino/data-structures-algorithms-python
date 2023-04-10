7773094a3c313f1c51c077089582fb18cd0b80f8
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-04-10 22:36:23 +0300

    Update anagrams_v1.py

--- arrays_and_strings/anagrams/anagrams_v1.py
+++ arrays_and_strings/anagrams/anagrams_v1.py
@@ -1,7 +1,7 @@
 def count_map(s):
     count_dict={}
     for char in s:
-        if char in count:
+        if char in count_dict:
             count_dict[char] += 1
         else:
             count_dict[char]=1
