e3cc6af3187a178bb8bc40db8ee2ab0a4a27c4a2
Agatha Bahati <agatha@marketforce360.com>
2023-04-11 03:10:53 +0300

    feat: add remove node task

---
+++ linked_lists/remove_node/remove_node.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def remove_node(head, target_val):
+    prev = None
+    current = head
+
+    if head.val == target_val:
+        return head.next
+    
+    while current is not None:
+        if current.val == target_val:
+            prev.next = current.next
+            break
+        
+        prev = current
+        current = current.next
+    
+    return head
