a1d9cab2958a2b0c729bf0b0214dedf4c0bab8fb
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-04-11 03:20:07 +0300

    feat: add remove node task v2 (#72)
    Co-authored-by: Agatha Bahati <agatha@marketforce360.com>

---
+++ linked_lists/remove_node/remove_node_v2.py
@@ -0,0 +1,9 @@
+def remove_node(head, target_val):
+    if head is None:
+        return None
+
+    if head.val == target_val:
+        return head.next
+
+    head.next = remove_node(head.next, target_val)
+    return head
