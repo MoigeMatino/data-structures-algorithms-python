77ca91e99f901b39e4c01af47c93b694bbda4b31
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-04-14 20:35:29 +0300

    feat: add insert node v1 (#93)
    Co-authored-by: Agatha Bahati <agatha@marketforce360.com>

---
+++ linked_lists/insert_node/insert_node_v1.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def insert_node(head, value, index):
+    if index == 0:
+        new_node = Node(value)
+        new_node.next = head
+        return new_node
+
+    count = 0
+    current = head
+    while current is not None:
+        if count == index-1:
+            temp = current.next
+            current.next = Node(value)
+            current.next.next = temp
+        count += 1
+        current = current.next
+    
+    return head
