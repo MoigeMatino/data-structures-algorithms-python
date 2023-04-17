c8cf9ce8418c541037b67cdabb413e1309ae0307
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-04-17 10:07:16 +0300

    Add depth first values node definition (#115)
    * feat: add depth first values v1
    
    * feat: add depth first values v1 Node definition
    
    ---------
    
    Co-authored-by: Agatha Bahati <agatha@marketforce360.com>

--- binary_tree/depth_first_values/depth_first_values_v1.py
+++ binary_tree/depth_first_values/depth_first_values_v1.py
@@ -1,3 +1,10 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
 def depth_first_values(root):
     stack = [root]
     values = []
