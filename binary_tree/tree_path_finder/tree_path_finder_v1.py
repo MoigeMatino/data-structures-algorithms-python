ffd191acdb8a732e14e41150b8bf761ca9b52b1a
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-04-27 21:11:14 +0300

    feat: add tree path finder task (#138)

---
+++ binary_tree/tree_path_finder/tree_path_finder_v1.py
@@ -0,0 +1,25 @@
+def path_finder(root, target):
+    if root is None:
+        return None
+    
+    if root.val == target:
+        return [root.val]
+    
+    left_path = path_finder(root.left, target)
+    if left_path is not None:
+        return [root.val, *left_path]
+    right_path = path_finder(root.right, target)
+    if right_path is not None:
+        return [root.val, *right_path]
+    
+    return None
+
+"""
+Depth first approach
+Less efficient solution due to the arrray operations in line 10 & 13
+
+n = number of nodes
+Time: O(n^2)
+Space: O(n)
+
+"""
