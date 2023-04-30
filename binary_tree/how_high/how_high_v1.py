54f032610efaedfc8eb24aa436f47ed440edb35b
Agatha Bahati <matinodev@gmail.com>
2023-04-30 20:59:16 +0300

    feat: add how high v1

---
+++ binary_tree/how_high/how_high_v1.py
@@ -0,0 +1,23 @@
+def how_high(node):
+    if node is None:
+        return -1
+    
+    stack = [node]
+    level=-1 #starting at -1 so that we can start counting the legitimate height from the children of the root
+    while stack:
+        
+        for x in stack:
+            if x.right:
+                stack.append(x.right)
+            if x.left:
+                stack.append(x.left)
+            stack.remove(x)
+        level += 1
+    return level
+
+"""
+DFS iterative approach
+n - number of nodes
+Time: O(n^2)
+Space: O(n)
+"""
