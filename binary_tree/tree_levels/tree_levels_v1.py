e5fb2c6b1b9d9f1c44b278012e4a3547adc87b7f
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-05-01 18:23:06 +0300

    Update tree_levels_v1.py

--- binary_tree/tree_levels/tree_levels_v1.py
+++ binary_tree/tree_levels/tree_levels_v1.py
@@ -31,4 +31,15 @@ n - number of nodes
 Time: O(n)
 Space: O(n)
 
+NB:
+Even though there is a nested for loop inside the while loop, the time complexity is still O(n) and not O(n^2).
+
+Here's why:
+
+    - The outer while loop runs at most n times because there are at most n nodes in the tree.
+    - The inner for loop runs at most n times, because it processes all the nodes on the current level of the tree, and there are at most n nodes in the tree.
+    - The number of times the inner for loop runs is proportional to the number of nodes in the tree, not the square of the number of nodes.
+
+So the overall time complexity of the tree_levels function is O(n), not O(n^2).
+
 """
