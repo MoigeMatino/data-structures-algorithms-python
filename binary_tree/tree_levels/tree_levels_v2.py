e46212841a1fcd659566b98fc1384b186e66e221
Agatha Bahati <matinodev@gmail.com>
2023-05-01 18:44:47 +0300

    feat: add tree levels v2

---
+++ binary_tree/tree_levels/tree_levels_v2.py
@@ -0,0 +1,33 @@
+from collections import deque
+    
+def tree_levels(root):
+    if root is None:
+        return []
+    
+    levels = []
+    queue = deque([ (root, 0) ])
+    while queue:
+        current, level_num = queue.popleft()
+        
+        if len(levels) == level_num:
+            levels.append([ current.val ])
+        else:
+            levels[level_num].append(current.val)
+        
+        if current.left is not None:
+            queue.append((current.left, level_num + 1))
+        
+        if current.right is not None:
+            queue.append((current.right, level_num + 1))
+    
+    return levels
+
+"""
+BFS iterative - Approach 2
+
+
+n = number of nodes
+Time: O(n)
+Space: O(n)
+
+"""
