e1616b405c3c165e2a1ad9d889b7369a83af2976
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-05-06 20:28:44 +0300

    feat: add undirected path v1 (#177)

---
+++ graphs/undirected_path/undirected_path_v1.py
@@ -0,0 +1,48 @@
+def undirected_path(edges, node_A, node_B):
+    graph = build_graph(edges)
+    return has_path(graph, node_A, node_B, set())
+
+def build_graph(edges):
+    graph = {}
+    for edge in edges:
+        a,b = edge
+        
+        if a not in graph:
+            graph[a] = []
+        
+        if b not in graph:
+            graph[b] = []
+        
+        graph[a].append(b)
+        graph[b].append(a)
+        
+    return graph
+
+def has_path(graph, src, dst, visited):
+    stack = [src]
+    
+    while stack:
+        current = stack.pop()
+        
+        if current == dst:
+            return True
+        
+        if current in visited:
+            continue
+        
+        visited.add(current)
+        
+        for neighbor in graph[current]:
+            stack.append(neighbor)
+        
+    return False
+    
+"""
+Depth first iterative
+
+n = number of nodes
+e = number edges
+Time: O(e)
+Space: O(e)
+
+"""
