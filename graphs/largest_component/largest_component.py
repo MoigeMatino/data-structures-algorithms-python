ef1e0dffdedd4644b27c03b04c6383e795c81e0b
Agatha Bahati <matinodev@gmail.com>
2023-05-09 23:51:36 +0300

    feat: add largest component task

---
+++ graphs/largest_component/largest_component.py
@@ -0,0 +1,23 @@
+def largest_component(graph):
+    visited = set()
+    
+    largest = 0
+    for node in graph:
+        size = explore_size(graph, node, visited)
+        if size > largest:
+            largest = size
+    
+    return largest
+
+def explore_size(graph, node, visited):
+    if node in visited:
+        return 0
+    
+    visited.add(node)
+    
+    size = 1
+    for neighbor in graph[node]:
+        size += explore_size(graph, neighbor, visited)
+        
+    return size
+
