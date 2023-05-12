cf1837947d4928acc273fac944cb14fa7ab39440
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-05-12 14:49:01 +0300

    Update undirected_path_v1.py

--- graphs/undirected_path/undirected_path_v1.py
+++ graphs/undirected_path/undirected_path_v1.py
@@ -46,6 +46,6 @@ Time: O(e)
 Space: O(e)
 
 NB: In this problem, it is very important to account for the presence of cycles with is a relevant/possible occurrence
-with undirected graphs.
+with undirected graphs. This keeps us from being trapped in an infinite loop.
 
 """
