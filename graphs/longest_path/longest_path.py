8aba03ec30259ef1b27c8d213d9b58c5ad15ec6d
Agatha Bahati <matinodev@gmail.com>
2023-05-18 14:26:40 +0300

    feat: add longest path task

---
+++ graphs/longest_path/longest_path.py
@@ -0,0 +1,36 @@
+def longest_path(graph):
+    distance = {}
+    
+    for node in graph:
+        if graph[node] == []:
+            distance[node] = 0
+        
+    for node in graph:
+        traverse_distance(graph, node, distance)
+        
+    return max(distance.values())
+
+def traverse_distance(graph, node, distance):
+    if node in distance:
+        return distance[node]
+    
+    largest = 0
+
+    for neighbor in graph[node]:
+        temp = traverse_distance(graph, neighbor, distance)
+        if temp > largest:
+            largest = temp
+    
+    distance[node] = 1+largest    
+    
+    return distance[node]
+
+"""
+
+e = # edges
+n = # nodes
+Time: O(e)
+Space: O(n)
+
+"""
+        
