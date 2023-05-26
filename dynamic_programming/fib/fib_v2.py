1cdea000c28c61ea2e9c6e52822ea3bf52a3789f
Agatha Bahati <matinodev@gmail.com>
2023-05-26 20:26:57 +0300

    feat: add fib v2

---
+++ dynamic_programming/fib/fib_v2.py
@@ -0,0 +1,20 @@
+def fib(n):
+    memo = {}
+    return _fib(n, memo)
+
+def _fib(n, memo):
+    if n == 0 or n == 1:
+        return n
+    
+    if n in memo:
+        return memo[n]
+
+    memo[n] = _fib(n - 1, memo) + _fib(n - 2, memo)
+    return memo[n]
+
+"""
+
+Time: O(n)
+Space: O(n)
+
+"""
