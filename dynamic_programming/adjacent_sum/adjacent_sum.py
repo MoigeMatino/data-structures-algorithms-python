a60a3685497480d86299234a91b0ec8e98b2fe1f
Agatha Bahati <matinodev@gmail.com>
2023-06-08 23:05:54 +0300

    doc: add adjacent sum documentation

--- dynamic_programming/adjacent_sum/adjacent_sum.py
+++ dynamic_programming/adjacent_sum/adjacent_sum.py
@@ -11,5 +11,12 @@ def _non_adjacent_sum(nums, i, memo):
     include = nums[i] + _non_adjacent_sum(nums, i + 2, memo)
     exclude = _non_adjacent_sum(nums, i + 1, memo)
     memo[i] = max(include, exclude)
-    
+
     return memo[i]
+
+"""
+n = length of nums
+Time: O(n)
+Space: O(n)
+
+"""
