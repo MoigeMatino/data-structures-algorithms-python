c1f5bb07a95a5f73f515d6a5c571f966d4d41a84
Agatha Bahati <matinodev@gmail.com>
2023-06-08 23:02:58 +0300

    feat: add adjacent sum task

---
+++ dynamic_programming/adjacent_sum/adjacent_sum.py
@@ -0,0 +1,15 @@
+def non_adjacent_sum(nums):
+    return _non_adjacent_sum(nums, 0, {})
+
+def _non_adjacent_sum(nums, i, memo):
+    if i in memo:
+        return memo[i]
+    
+    if i >= len(nums):
+        return 0
+    
+    include = nums[i] + _non_adjacent_sum(nums, i + 2, memo)
+    exclude = _non_adjacent_sum(nums, i + 1, memo)
+    memo[i] = max(include, exclude)
+    
+    return memo[i]
