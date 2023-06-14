1d7b53a35db03c14a85bf245cdc62154fac55295
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2023-06-14 15:27:57 +0300

    feat: add counting change task (#243)

---
+++ dynamic_programming/counting_change/counting_change.py
@@ -0,0 +1,26 @@
+def counting_change(amount, coins):
+    return _counting_change(amount, coins, 0, {})
+
+
+def _counting_change(amount, coins, i, memo):
+    key = amount, i
+
+    if key in memo:
+        return memo[key]
+    
+    if amount == 0:
+        return 1
+    
+    if i == len(coins):
+        return 0
+    
+    total_ways = 0
+    coin = coin[i]
+
+    for qty in range(0, (amount//coin)+1):
+        remainder = amount - (qty*coin)
+        total_ways += _counting_change(remainder, coins, i+1, memo)
+
+    memo[key] = total_ways
+
+    return total_ways
