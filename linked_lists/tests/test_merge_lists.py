bf6db9758b482f08c9cef3611cf93f78048cdaf6
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-01-08 22:45:42 +0300

    test: add merge_lists test 2 (#271)

--- linked_lists/tests/test_merge_lists.py
+++ linked_lists/tests/test_merge_lists.py
@@ -32,3 +32,31 @@ def test_merge_lists_5_6():
     merge_lists_head_v1 = merge_lists_v1(a, q)
     assert merge_lists_head_v1.val == test_case[0]
     assert merge_lists_head_v1.next.val == 6
+
+def test_merge_lists_5_1():
+    a = Node(5)
+    b = Node(7)
+    c = Node(10)
+    d = Node(12)
+    e = Node(20)
+    f = Node(28)
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
+    e.next = f
+    
+
+    q = Node(1)
+    r = Node(8)
+    s = Node(9)
+    t = Node(10)
+    q.next = r
+    r.next = s
+    s.next = t
+
+    merge_lists_head_v1 = merge_lists_v1(a, q)
+    assert merge_lists_head_v1.val == test_case[1]
+    assert merge_lists_head_v1.next.val == 5
+
+    
