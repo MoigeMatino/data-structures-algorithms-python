4518f4c64f24e740e051e61013b7ad22c8cba7b6
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-01-09 11:34:28 +0300

    Update compress_v2.py

--- arrays_and_strings/compress/compress_v2.py
+++ arrays_and_strings/compress/compress_v2.py
@@ -30,5 +30,5 @@ Space complexity: O(n)
 The time complexity of this algorithm is linear time because we run n iterations. 
 To note is that appending an item to the end of a list is a constant time operation.
 
-To note: Adding the '!' token alllows us to access the last set of characters that are similar.
+To note: Adding the '!' token allows us to access the last set of characters that are similar.
 """
