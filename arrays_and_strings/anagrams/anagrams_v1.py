edee2829688adfb86dbc815f789d42b6731cdde6
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-01-09 12:04:33 +0300

    feat: add anagrams_v2 complexities (#276)

--- arrays_and_strings/anagrams/anagrams_v1.py
+++ arrays_and_strings/anagrams/anagrams_v1.py
@@ -10,3 +10,12 @@ def count_map(s):
 
 def anagrams(s1,s2):
     return count_map(s1) == count_map(s2)
+
+"""
+n = length of string 1
+m = length of string 2
+
+Time: O(n + m)
+Space: O(n + m)
+
+"""
