1e7e296dbfc5c5bf9dbb363bb0e56a6c20a70a57
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-01-09 15:57:04 +0300

    feat: add pair_product_v2 (#278)

--- arrays_and_strings/tests/test_pair_product.py
+++ arrays_and_strings/tests/test_pair_product.py
@@ -1,4 +1,6 @@
 from pair_product.pair_product_v1 import pair_product
+from pair_product.pair_product_v2 import pair_product_v2
+
 
 test_case = [
     ([3, 2, 5, 4, 1], 8,(1,3)),
@@ -11,3 +13,4 @@ test_case = [
 def test_pair_product():
     for test in test_case:
         assert pair_product(test[0],test[1])==test[2]
+        assert pair_product_v2(test[0],test[1]) == test[2] or test[2][::-1]
