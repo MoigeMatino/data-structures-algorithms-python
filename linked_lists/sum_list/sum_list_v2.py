bac7142d27902854ffa9e28cbf96b29ac3e0c4ba
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-01-10 11:46:01 +0300

    Update sum_list_v2.py

--- linked_lists/sum_list/sum_list_v2.py
+++ linked_lists/sum_list/sum_list_v2.py
@@ -6,5 +6,8 @@ def sum_list(head):
 """
 n: number of nodes in linked list
 Time: O(n)
-Space: O(n); this is because of the memory 
+Space: O(n); this is because of the call stack utilised by recursive algorithms
+to manage function calls. Every time the recursive function is called a new stack
+frame is added to the call stack, storing with it information about the function 
+call, local variables and return addresses.
 """
