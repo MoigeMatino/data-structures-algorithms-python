913da3a05af51d620683ec4e399ffa04ead90338
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-01-22 10:45:47 +0300

    Update bottom_right_value.py (#283)

--- binary_tree/bottom_right_value/bottom_right_value.py
+++ binary_tree/bottom_right_value/bottom_right_value.py
@@ -15,7 +15,17 @@ def bottom_right_value(root):
     return current.val
 
 """
-Tip: the last left most node with no children will represent the right most node, remember that we're working with a queue.
+The reason we use BFS and not DFS is because DFS will get the right most value,
+which may not necessarily be the BOTTOM right value. Consider this tree, DFS would return -13 
+but the correct answer is 6. BFS will return the correct answer because of the way it traverses
+the tree; level by level:
+#        -1
+#      /   \
+#    -6    -5
+#   /  \     \
+# -3   -4   -13
+#     / \       
+#    -2  6
 BFS iterative approach
 
 n = number of nodes
