be8c1de8b74a1e5b1260bf28b4c0b262c2a694a2
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-01-23 22:03:45 +0300

    Update bottom_right_value.py

--- binary_tree/bottom_right_value/bottom_right_value.py
+++ binary_tree/bottom_right_value/bottom_right_value.py
@@ -15,6 +15,7 @@ def bottom_right_value(root):
     return current.val
 
 """
+BFS iterative approach
 The reason we use BFS and not DFS is because DFS will get the right most value,
 which may not necessarily be the BOTTOM right value. Consider this tree, DFS would return -13 
 but the correct answer is 6. BFS will return the correct answer because of the way it traverses
@@ -26,7 +27,6 @@ the tree; level by level:
 # -3   -4   -13
 #     / \       
 #    -2  6
-BFS iterative approach
 
 n = number of nodes
 Time: O(n)
