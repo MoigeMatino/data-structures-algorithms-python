c7f941d1dc8fd21228fe34ae639e425bb2b481c2
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-02-12 11:43:13 +0300

    Update connected_components_v1.py

--- graphs/connected_components/connected_components_v1.py
+++ graphs/connected_components/connected_components_v1.py
@@ -28,4 +28,19 @@ e = number edges
 Time: O(e)
 Space: O(n)
 
+This algorithm can be broken down into two functions:
+1. Function to iterate over every node in the graph so 
+    as to start a traversal from each of those nodes.
+
+2. Function that traverses the current node in the graph 
+    and explores it to find the number of nodes connected 
+    to it to make a connected component.
+
+Note that the base case in the recursive function is implicit.
+i.e by virtue of line 18: 'for neighbor in graph[current]', it 
+implies that the recursive call will only be made if the current
+node has neighbor. In the event that the neighbor is absent, the 
+recursive call is not made; the base case is the absence of a recursive
+call/case.
+
 """
