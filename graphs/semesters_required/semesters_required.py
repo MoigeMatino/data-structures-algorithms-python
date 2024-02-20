8b00e850124df514afbf4f70150caa39bc10e52c
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-02-20 13:56:33 +0300

    Update semesters_required.py

--- graphs/semesters_required/semesters_required.py
+++ graphs/semesters_required/semesters_required.py
@@ -1,38 +1,39 @@
 def semesters_required(num_courses, prereqs):
-    distance = {}
+    graph = build_graph(prereqs, num_courses)
+    no_of_semesters = {}
+    # initiate the no_of_semesters dict with the terminal courses
+    for course in graph:
+        if graph[course] == []:
+            no_of_semesters[course] = 1
+            
+    # start traversing the graph from each course
+    for course in graph:
+        get_semesters_required(course, graph, no_of_semesters)
     
-    graph = build_graph(num_courses, prereqs)
-    
-    for node in graph:
-        if len(graph[node]) == 0:
-            distance[node] = 1
-        
-    for node in graph:    
-        traversal_distance(graph, node, distance)
-        
-    return max(distance.values())
+    #return the semesters_required
+    return max(no_of_semesters.values())
     
 def build_graph(prereqs, num_courses):
+    # build the directed acyclic graph
     graph = {course_id: [] for course_id in range(num_courses)}
     for course_a, course_b in prereqs:
-        graph[course_a].append(course_b)            
+        graph[course_a].append(course_b)
             
     return graph
-
-def traversal_distance(graph, node, distance):
-    if node in distance:
-        return distance[node]
     
-    max = 0
+def get_semesters_required(course, graph, no_of_semesters):
+    if course in no_of_semesters:
+        return no_of_semesters[course]
+        
+    maximum = 0
+    for neighbor_course in graph[course]:
+        temp_max = get_semesters_required(neighbor_course, graph, no_of_semesters)
+        if temp_max > maximum:
+            maximum = temp_max
+            
+    no_of_semesters[course] = 1 + maximum
     
-    for neighbor in graph[node]:
-        temp = traversal_distance(graph, neighbor, distance)
-        if temp > max:
-            max = temp
-    
-    distance[node] = 1 + max
-    
-    return distance[node]
+    return no_of_semesters[course]
 
 """
 
