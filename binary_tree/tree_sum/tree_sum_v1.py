0c39764651ac79fb69fa03d6f6cda48d372a1763
Agatha Bahati <58204713+MoigeMatino@users.noreply.github.com>
2024-03-04 22:56:56 +0300

    doc: enahnce tree_sum_v1.py commentary

--- binary_tree/tree_sum/tree_sum_v1.py
+++ binary_tree/tree_sum/tree_sum_v1.py
@@ -1,27 +1,46 @@
 from collections import deque
 
 def tree_sum(root):
-    if not root:
+    """
+    Calculates the sum of all node values in a binary tree using breadth-first traversal.
+
+    Args:
+        root (Node): The root node of the binary tree.
+
+    Returns:
+        int: The sum of all node values in the binary tree.
+
+    """
+    if not root:  # If the tree is empty, return 0
         return 0
     
-    sum = 0
-    queue = deque([root])
-    while queue:
-        current = queue.popleft()
-        sum += current.val
+    sum = 0  # Initialize the sum of node values
+    queue = deque([root])  # Initialize deque with the root node
+
+    while queue:  # Continue traversal until the deque is empty
+        current = queue.popleft()  # Dequeue the leftmost node from the deque
+        sum += current.val  # Add the value of the dequeued node to the sum
         
-        if current.left:
+        if current.left:  # If the dequeued node has a left child, enqueue it
             queue.append(current.left)
         
-        if current.right:
+        if current.right:  # If the dequeued node has a right child, enqueue it
             queue.append(current.right)
         
-    return sum
+    return sum  # Return the sum of all node values
 
 """
-Breadth first approach
-n -  # of nodes
-Time: O(n)
-Space: O(n)
+Time Complexity Analysis:
+- Enqueuing and dequeuing nodes: O(n), where n is the number of nodes in the binary tree.
+Overall: O(n)
 
+Space Complexity Analysis:
+- Storing nodes in the deque: O(n), where n is the number of nodes in the binary tree.
+Overall: O(n)
+
+Further Notes:
+This algorithm calculates the sum of all node values in a binary tree using breadth-first traversal. 
+It starts from the root node and enqueues nodes at each level, and dequeues nodes in the order of 
+their arrival, exploring nodes level by level. The values of the visited nodes are added to the sum, 
+and the final sum is returned. The time complexity is O(n), and the space complexity is O(n).
 """
