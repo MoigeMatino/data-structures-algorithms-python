ea4fbccfac9e3a49e669c4283dfbc1613c3c04cd
Agatha Bahati <matinodev@gmail.com>
2024-03-18 21:41:12 +0300

    feat: Add decompress braces solution

---
+++ stacks/decompress_braces/decompress_braces.py
@@ -0,0 +1,17 @@
+def decompress_braces(string):
+    numbers = '123456789'
+    stack = []
+    for char in string:
+        if char in numbers:
+            stack.append(int(char))
+        else:
+            if char == '}':
+                segment = ''
+                while isinstance(stack[-1], str):
+                    popped = stack.pop()
+                    segment = popped + segment
+                num = stack.pop()
+                stack.append(segment * num)
+            elif char != '{':
+                stack.append(char)
+    return ''.join(stack)
