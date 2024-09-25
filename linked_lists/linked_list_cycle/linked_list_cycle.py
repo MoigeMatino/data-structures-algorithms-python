f3024b8106c65febcd3b5073f385b4a37a52d439
AgathaBahati <matinodev@gmail.com>
2024-09-25 08:03:11 +0300

    feat: add linked list cycle

---
+++ linked_lists/linked_list_cycle/linked_list_cycle.py
@@ -0,0 +1,15 @@
+def detect_cycle(head):
+    if head is None:
+        return None
+    
+    slow = head
+    fast = head
+    
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        
+        if slow == fast:
+            return True
+    
+    return False
